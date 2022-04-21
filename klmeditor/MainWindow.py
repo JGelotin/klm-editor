2ae65a4c003088382ab5356347b64f060ab78697
GuanlinWang <gary.wang@csu.fullerton.edu>
2022-04-21 13:09:24 -0700

    Added import and query functionality for database files

--- klmeditor/MainWindow.py
+++ klmeditor/MainWindow.py
@@ -3,6 +3,7 @@ import os
 import csv
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import *
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery, QSqlTableModel
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,9 +13,14 @@ class MainWindow(QMainWindow):
         self.databaseTableComboBox.setVisible(False)
         self.show()
 
+        # Database
+        self.db = None
+
         # Models are used to manage the data and the data is displayed using the TableView widget
-        # Setting to none for now. Maybe use of QSqlTableModel later?
         #
+        # model is default set to None since there are two models that
+        # can be used in the program. (QStandardItemModel and QSqlTableModel)
+
         # QSortFilterProxyModel allows for filtered data to be edited while maintaining the original model using a proxy model
         # The proxy model can be filtered and the data edited in the filtered table is reflected on the original model without
         # having to rewrite the whole model.
@@ -25,9 +31,10 @@ class MainWindow(QMainWindow):
         self.openButton.clicked.connect(self.open_file_dialog)
         self.exitButton.clicked.connect(self.close)
         self.filterButton.clicked.connect(self.filter_table)
+        self.queryButton.clicked.connect(self.execute_query)
         
     def open_file_dialog(self):
-        filename = QFileDialog.getOpenFileName(filter="CSV File (*.csv)")
+        filename = QFileDialog.getOpenFileName(filter="CSV File (*.csv);;Database File (*.db)")
         split_filename = os.path.splitext(filename[0])
 
         file_name = split_filename[0]
@@ -56,6 +63,37 @@ class MainWindow(QMainWindow):
 
                     # Model is displayed using the proxy model
                     self.tableView.setModel(self.filter_proxy_model)
+            
+            elif file_extension == ".db":
+                # Disable csv related UI elements
+                self.filterButton.setVisible(False)
+                self.queryButton.setVisible(True)
+                self.databaseTableComboBox.setVisible(True)
+                self.inputLine.setGeometry(150, 100, 441, 41)
+
+                self.db = QSqlDatabase.addDatabase("QSQLITE")
+                self.db.setDatabaseName(file_name + file_extension)
+
+                if self.db.open():
+                    self.model = QSqlTableModel()
+
+                    # First table in database is open and displayed by default
+                    statement = "SELECT * FROM " + self.db.tables()[0]
+                    query = QSqlQuery(statement)
+                    self.model.setQuery(query)
+
+                    self.tableView.setModel(self.model)
+
+    def execute_query(self):
+        if type(self.model) == type(QSqlTableModel()):
+            # Executes query from input line
+            query = QSqlQuery(self.inputLine.text())
+            self.model.setQuery(query)
+
+    def execute_query(self, statement):
+        if type(self.model) == type(QSqlTableModel()):
+            query = QSqlQuery(statement)
+            self.model.setQuery(query)
 
     def fill_model_from_text(self, data):
         for x in range(0, len(data)):
